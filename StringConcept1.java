c020377dab1692334609f6f665af13bedef94631
Ishan <106976566+Ishann17@users.noreply.github.com>
2024-04-13 13:03:12 +0530

    Minor changes in the lines

--- StringConcept1.java
+++ StringConcept1.java
@@ -8,7 +8,9 @@ public class StringConcept1 {
      ta.replace("C", "X");
      ta = ta.concat(tb);
      System.out.println(ta + "\n");
-
+        
+    //another concept
+        
      StringBuilder sb = new StringBuilder("B");
      sb.append(sb.append("A"));
      System.out.println(sb);
@@ -18,6 +20,6 @@ public class StringConcept1 {
 
 //LINE 8 will replace the letters during runtime and create a new String Object, but we have not stored it anywhere.
 /*
-LINE 13 We are using to append function and evaluation will start from bracket. First it will do Append on "B" and "A" and create "BA".
-then it will again append on sb(BA). BA.append(BA). Therefore answer is BABA :)
+LINE 15 We are using to append function and evaluation will start from bracket. First, it will Append "B" and "A" and create "BA".
+then it will again append on sb--->BA. that is BA.append(BA). Therefore the answer is BABA :)
 */
